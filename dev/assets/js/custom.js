475f85808a2486eaa85c9aca8ce93aadebf394a6
S1imShot <slimshot123@gmail.com>
2023-10-28 18:31:03 +0300

    fix

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,3 +1,10 @@
+$(document).ready(function () {
+	$(".first-screen-slider").slick({
+		dots: true,
+		arrows: false,
+	});
+});
+
 const burger = document.querySelector(".hamburger");
 burger.addEventListener("click", function () {
 	burger.classList.toggle("is-active");
